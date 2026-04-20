297c1b60d42cb20e7c1cd8c19845e74d8a56dc1e
Zack <zacharycummings0714@gmail.com>
2026-04-19 19:55:39 -0600

    I can follow a tutorial!

---
+++ TestMod/TestMod/TestMod.cs
@@ -0,0 +1,13 @@
+﻿using ILogger = Core.Logging.ILogger;
+
+public class TestMod : IMod
+{
+    public TestMod(ILogger logger)
+    {
+        logger.Info?.Log("Hello, shapez 2");
+    }
+
+    public void Dispose()
+    {
+    }
+}
