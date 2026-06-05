b7b1c6140d022939f829ae11f67b6e84223adac3
Zack <zacharycummings0714@gmail.com>
2026-06-05 02:15:36 -0600

    HybridStop: Unloader functionality

--- HybridStop/HybridStopDecider.cs
+++ HybridStop/HybridStopDecider.cs
@@ -140,60 +140,92 @@ namespace HybridStop
         private bool TrainHasCompleteExchanges(TrainId trainId, TrainSimulationData trainSimulationData)
         {
             // return true if there is a layer of any wagon that cannot exchange, and the layer of the exchanger is ENABLED.
-
             Logger.Info?.Log($"checking if train {trainId} has any complete exchanges");
 
             for (int wagonIndex = 1; wagonIndex < trainSimulationData.Wagons.Length; wagonIndex++)
             {
                 Logger.Info?.Log($"checking wagon {wagonIndex}");
                 WagonNavigationData wagonNavigationData = trainSimulationData.Wagons[wagonIndex];
-                if (!wagonNavigationData.UpsideDown)
-                {
-                    GlobalChunkCoordinate position = wagonNavigationData.Outgoing.Position;
-                    if (!CargoSimulator.TryGetExchanger(position, out ICargoExchanger cargoExchanger))
-                    {
-                        break;
-                    }
+                if (wagonNavigationData.UpsideDown)
+                    continue;
 
-                    TrainWagonId trainWagonId = Simulation.FindTrainWagonByIndex_Slow(trainId, wagonIndex);
-                    CargoSimulator.TryGetCargo(trainWagonId, out IWagonCargoData wagonCargoData);
-                    Logger.Info?.Log($"wagonCargoData type: {wagonCargoData.GetType()}");
-                    if (wagonCargoData is LayeredWagonCargo<CargoContainer<FluidId>> fluidWagonCargoData)
-                    {
-                        Logger.Info?.Log("wagon is fluid wagon");
-                        // see if it's an unloader or a loader, then see if the wagon is empty/full
-                        if (cargoExchanger is TrainCargoUnloaderSimulation<FluidId> fluidUnloader)
-                        {
-                            Logger.Info?.Log("exchanger is fluid unloader");
-                            for (int j = 0; j < fluidWagonCargoData.Containers.Count; j++)
-                            {
-                                Logger.Info?.Log($"container {j}: is active {fluidUnloader.IsLayerActive(j)}, is empty {fluidWagonCargoData.Containers[j].IsEmpty}, can accept {fluidUnloader.BridgeLanes[j].CanAcceptContainer()}");
-                                if (fluidUnloader.IsLayerActive(j) && !fluidWagonCargoData.Containers[j].IsEmpty)
-                                {
-                                    int num = fluidUnloader.BridgeLanes[j].ContainerCount() + fluidUnloader.ContainerTracks[j].ContainerCount() + 1;
-                                    if (!fluidUnloader.BridgeLanes[j].CanAcceptContainer() || num > fluidUnloader.ContainerTracks[j].MaxContainersOnTrack)
-                                    {
-                                        return true;
-                                    }
-                                }
-                                else if (fluidWagonCargoData.Containers[j].IsEmpty)
-                                {
-                                    return true;
-                                }
-                            }
-                        }
-                        else if (cargoExchanger is TrainCargoLoaderSimulation<FluidId> fluidLoader)
-                        {
-                                
-                        }
-                    }
-                    else if (wagonCargoData is LayeredWagonCargo<ShapeId> shapeCargo)
-                    {
-                            
-                    }
+                GlobalChunkCoordinate position = wagonNavigationData.Outgoing.Position;
+                if (!CargoSimulator.TryGetExchanger(position, out ICargoExchanger cargoExchanger))
+                    break;
+
+                TrainWagonId trainWagonId = Simulation.FindTrainWagonByIndex_Slow(trainId, wagonIndex);
+                CargoSimulator.TryGetCargo(trainWagonId, out IWagonCargoData wagonCargoData);
+                Logger.Info?.Log($"wagonCargoData type: {wagonCargoData.GetType()}");
+
+                // capture both typed exchanger and typed cargo in the same pattern-match to avoid repeated casts
+                if (cargoExchanger is TrainCargoUnloaderSimulation<FluidId> fluidUnloader &&
+                    wagonCargoData is LayeredWagonCargo<CargoContainer<FluidId>> fluidWagonCargo)
+                {
+                    Logger.Info?.Log("wagon is fluid wagon");
+                    Logger.Info?.Log("exchanger is fluid unloader");
+                    if (IsUnloaderBlocking(fluidUnloader, fluidWagonCargo))
+                        return true;
+                }
+                else if (cargoExchanger is TrainCargoLoaderSimulation<FluidId> fluidLoader &&
+                         wagonCargoData is LayeredWagonCargo<CargoContainer<FluidId>> fluidWagonCargo2)
+                {
+                    if (IsLoaderBlocking(fluidLoader, fluidWagonCargo2))
+                        return true;
+                }
+                else if (cargoExchanger is TrainCargoLoaderSimulation<ShapeId> shapeLoader && 
+                    wagonCargoData is LayeredWagonCargo<CargoContainer<ShapeId>> shapeCargo)
+                {
+                    if (IsLoaderBlocking(shapeLoader, shapeCargo))
+                        return true;
+                }
+                else if (cargoExchanger is TrainCargoUnloaderSimulation<ShapeId> shapeUnloader &&
+                    wagonCargoData is LayeredWagonCargo<CargoContainer<ShapeId>> shapeCargo2)
+                {
+                    if (IsUnloaderBlocking(shapeUnloader, shapeCargo2))
+                        return true;
+                }
+            }
+
+            return false;
+        }
+
+        private bool IsUnloaderBlocking<TItem>(TrainCargoUnloaderSimulation<TItem> unloader, LayeredWagonCargo<CargoContainer<TItem>> wagonCargo)
+            where TItem : unmanaged, IEquatable<TItem>
+        {
+            for (int j = 0; j < wagonCargo.Containers.Count; j++)
+            {
+                Logger.Info?.Log($"container {j}: is active {unloader.IsLayerActive(j)}, is empty {wagonCargo.Containers[j].IsEmpty}, can accept {unloader.BridgeLanes[j].CanAcceptContainer()}");
+                if (unloader.IsLayerActive(j) && !wagonCargo.Containers[j].IsEmpty)
+                {
+                    int num = unloader.BridgeLanes[j].ContainerCount() + unloader.ContainerTracks[j].ContainerCount() + 1;
+                    if (!unloader.BridgeLanes[j].CanAcceptContainer() || num > unloader.ContainerTracks[j].MaxContainersOnTrack)
+                        return true;
+                }
+                else if (wagonCargo.Containers[j].IsEmpty)
+                {
+                    return true;
                 }
             }
             return false;
         }
+
+        private bool IsLoaderBlocking<TItem>(TrainCargoLoaderSimulation<TItem> loader, LayeredWagonCargo<CargoContainer<TItem>> wagonCargo) where TItem : unmanaged, IEquatable<TItem>
+        {
+            // TODO: does not catch all cases
+            for (int i = 0; i < wagonCargo.Containers.Count; i++)
+            {
+                CargoContainer<TItem> cargoContainer = wagonCargo.Containers[i];
+                if (cargoContainer.MaxPackages == loader.ContainerCapacityProvider.MaxPackagesPerContainer)
+                {
+                    return true;
+                }
+                if (cargoContainer.IsFull && loader.IsLayerActive(i))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
     }
 }
