47386e52e29023ffaa9b4a5b163617e57ab20673
Zack <zacharycummings0714@gmail.com>
2026-06-04 19:45:43 -0600

    UnlimitedWaitStop: Placeholder configuration setup

---
+++ UnlimitedWaitStop/WaitStopIslandConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using Game.Core.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnlimitedWaitStop
+{
+    public class WaitStopIslandConfiguration : IIslandConfiguration, IEntityConfiguration, IEquatable<IEntityConfiguration>
+    {
+        public int WaitTimeSeconds = -1;
+
+        public WaitStopIslandConfiguration() { }
+
+        public bool Equals(IEntityConfiguration other)
+        {
+            return other is WaitStopIslandConfiguration otherConfig && WaitTimeSeconds == otherConfig.WaitTimeSeconds;
+        }
+
+        public void Sync(ISerializationVisitor visitor)
+        {
+            visitor.SyncInt_4(ref WaitTimeSeconds);
+        }
+    }
+}
---
+++ UnlimitedWaitStop/WaitStopModuleProvider.cs
@@ -0,0 +1,38 @@
+﻿using Core.Localization;
+using Game.Core.Coordinates;
+using System;
+using System.Collections.Generic;
+
+namespace UnlimitedWaitStop
+{
+    public class WaitStopModuleProvider : IIslandModuleDataProvider
+    {
+        public IEnumerable<IHUDSidePanelModuleData> GetModules(IslandModel island)
+        {
+            IIslandConfiguration configuration = island.Configuration;
+            WaitStopIslandConfiguration config = configuration as WaitStopIslandConfiguration;
+            if (config == null)
+            {
+                yield break;
+            }
+
+            var transform = island.Transform;
+            GlobalChunkCoordinate stationChunk = ChunkVector.Zero.ToGlobal(in transform);
+
+            yield return new HUDSidePanelModuleInfoText.Data(new RawText("Wait Time: " + config.WaitTimeSeconds + " seconds"));
+            yield return new HUDSidePanelModuleGenericButton.Data("global.btn-configure".T(), () =>
+            {
+                ShowChannelConfigDialog(config, stationChunk);
+            });
+        }
+
+        public IEnumerable<IHUDSidePanelModuleData> GetStats()
+        {
+            yield break;
+        }
+
+        private void ShowChannelConfigDialog(WaitStopIslandConfiguration config, GlobalChunkCoordinate stationChunk)
+        {
+
+        }
+}
