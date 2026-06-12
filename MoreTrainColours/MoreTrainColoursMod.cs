93e65a1c08d3b09b7e9d14af0f300e0ca9e663cd
Zack <zacharycummings0714@gmail.com>
2026-06-12 03:19:35 -0600

    MoreTrainColours: Add black rail colour back-end

--- MoreTrainColours/MoreTrainColoursMod.cs
+++ MoreTrainColours/MoreTrainColoursMod.cs
@@ -1,4 +1,6 @@
-﻿using Core.Logging;
+﻿using MonoMod.RuntimeDetour;
+using ShapezShifter.Hijack;
+using ILogger = Core.Logging.ILogger;
 
 namespace MoreTrainColours
 {
@@ -6,9 +8,14 @@ namespace MoreTrainColours
     {
         public static ILogger Logger = null!;
 
+        // hooks
+        private Hook? _gameScenarioHook;
+
         public MoreTrainColoursMod(ILogger logger)
         {
             Logger = logger;
+
+            GameRewirers.AddRewirer(new MoreTrainColoursScenarioRewirer());
         }
 
         public void Dispose() { }
---
+++ MoreTrainColours/MoreTrainColoursScenarioRewirer.cs
@@ -0,0 +1,30 @@
+﻿using Game.Core.Rails;
+using ShapezShifter.Hijack;
+using UnityEngine;
+
+namespace MoreTrainColours
+{
+    public class MoreTrainColoursScenarioRewirer : IGameScenarioRewirer
+    {
+        public GameScenario ModifyGameScenario(GameScenario gameScenario)
+        {
+            MoreTrainColoursMod.Logger.Info?.Log($"Registering black rail color for scenario {gameScenario.UniqueId}...");
+            SerializableRailColor blackSerializableRailColor = new()
+            {
+                Id = new SerializableRailColorId("black"),
+                Tint = Color.black
+            };
+
+            MoreTrainColoursMod.Logger.Info?.Log("Attempting to register...");
+            if (!gameScenario.RailColorRegistry.TryRegisterColor(blackSerializableRailColor.Id, blackSerializableRailColor.Tint, out _))
+            {
+                MoreTrainColoursMod.Logger.Error?.Log($"Failed to register black rail color with id {blackSerializableRailColor.Id}");
+            }
+            else
+            {
+                MoreTrainColoursMod.Logger.Info?.Log($"Successfully registered black rail color for scenario {gameScenario.UniqueId}.");
+            }
+            return gameScenario;
+        }
+    }
+}
