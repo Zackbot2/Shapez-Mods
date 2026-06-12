171830c0e4da33de1d2f86e4d7d7d71768c9a5c5
Zack <zacharycummings0714@gmail.com>
2026-06-12 01:19:51 -0600

    MoreTrainColours: Initial commit

---
+++ MoreTrainColours/MoreTrainColoursMod.cs
@@ -0,0 +1,16 @@
+﻿using Core.Logging;
+
+namespace MoreTrainColours
+{
+    public class MoreTrainColoursMod : IMod
+    {
+        public static ILogger Logger = null!;
+
+        public MoreTrainColoursMod(ILogger logger)
+        {
+            Logger = logger;
+        }
+
+        public void Dispose() { }
+    }
+}
