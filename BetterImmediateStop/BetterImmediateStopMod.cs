d8728a08abd1e0526d3455cf04a96f4007ee0c7f
Zack <zacharycummings0714@gmail.com>
2026-06-12 14:36:59 -0600

    BetterImmediateStop: Configurable wait time

--- BetterImmediateStop/BetterImmediateStopMod.cs
+++ BetterImmediateStop/BetterImmediateStopMod.cs
@@ -1,6 +1,8 @@
-﻿using Game.Core.Trains;
-using Game.Core.Coordinates;
+﻿using Game.Core.Coordinates;
+using Game.Core.Simulation;
+using Game.Core.Trains;
 using MonoMod.RuntimeDetour;
+using Shapez2ModConfig;
 using ShapezShifter.SharpDetour;
 using ILogger = Core.Logging.ILogger;
 
@@ -8,26 +10,39 @@ namespace BetterImmediateStop
 {
     public class BetterImmediateStopMod : IMod
     {
-        private readonly ILogger _logger;
+        public static ILogger Logger = null!;
+
+        // config
+        public static ModConfig Config { get; private set; } = null!;
+        public static int DefaultWaitSeconds => Config.GetEntry<int>(WAIT_TIME_ID).Value;
+        private const string WAIT_TIME_ID = "maximum wait time";    // DO NOT CHANGE
+        private const string CONFIG_ID = "Zackbot2.BetterImmediateStop";   // DO NOT CHANGE
+
+        public static int WaitTimeSeconds => Config.GetEntry<int>(WAIT_TIME_ID).Value;
+        public static Ticks WaitTimeTicks => Ticks.FromSeconds(WaitTimeSeconds);
 
         // store the hook so it doesn't get GCed
         private readonly Hook shouldTrainLeaveHook;
 
         public BetterImmediateStopMod(ILogger logger)
         {
-            _logger = logger;
+            Logger = logger;
+
+            Config = new(CONFIG_ID, GetType());
+
+            Config.RegisterEntry<int>(WAIT_TIME_ID, 5).OnChanged.Register(value => Logger.Info?.Log($"Config entry \"{WAIT_TIME_ID}\" updated to {value}"));
+            Config.Load();
+            Config.Save();
 
             shouldTrainLeaveHook = CreateHook();
 
-            _logger.Info?.Log("BetterImmediateStop loaded successfully.");
+            Logger.Info?.Log("BetterImmediateStop loaded successfully.");
         }
 
         private Hook CreateHook()
         {
-            return DetourHelper.Replace<QuickStopDecider, TrainId, TrainSimulationData, bool>(
-                (quickStopDecider, id, trainSimulationData) => 
-                    quickStopDecider.ShouldTrainLeave(id, trainSimulationData)
-                    , QuickStop_ShouldTrainLeave);
+            return DetourHelper.Replace<QuickStopDecider, TrainId, TrainSimulationData, bool>((quickStopDecider, id, trainSimulationData) => 
+                quickStopDecider.ShouldTrainLeave(id, trainSimulationData), QuickStop_ShouldTrainLeave);
         }
 
         private static bool QuickStop_ShouldTrainLeave(QuickStopDecider deciderInstance, TrainId id, TrainSimulationData trainSimulationData)
@@ -37,7 +52,7 @@ namespace BetterImmediateStop
                 return false;
             }
 
-            if (deciderInstance.TrainSimulationTimeProvider.SimulationTime - trainSimulationData.StopTime > deciderInstance.TicksToWait)
+            if (deciderInstance.TrainSimulationTimeProvider.SimulationTime - trainSimulationData.StopTime > WaitTimeTicks)
             {
                 // force cancel all exchanges. LEAVE.
                 for (int i = 1; i < trainSimulationData.Wagons.Length; i++)
@@ -57,6 +72,9 @@ namespace BetterImmediateStop
                 && !deciderInstance.TrainCanExchangeImmediately(id, trainSimulationData);
         }
 
-        public void Dispose() { }
+        public void Dispose() 
+        {
+            shouldTrainLeaveHook?.Dispose();
+        }
     }
 }
