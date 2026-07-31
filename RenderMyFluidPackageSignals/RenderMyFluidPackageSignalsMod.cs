9867378ebedf8c15ece66ca0f5c2c3e065b82c4d
Zack <zacharycummings0714@gmail.com>
2026-07-31 02:33:34 -0600

    Create RenderMyFluidPackageSignals mod

---
+++ RenderMyFluidPackageSignals/RenderMyFluidPackageSignalsMod.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Reflection;
+using Game.Content.Features.Signals;
+using MonoMod.RuntimeDetour;
+using ShapezShifter.SharpDetour;
+using ILogger = Core.Logging.ILogger;
+
+namespace RenderMyFluidPackageSignals
+{
+    public class RenderMyFluidPackageSignalsMod
+    {
+        internal static ILogger Logger { get; private set; } = null!;
+
+        // store hooks so they don't get GCed, and so we can dispose them later
+        private Hook? _renderSignalHook = null;
+
+        public RenderMyFluidPackageSignalsMod(ILogger logger)
+        {
+            MethodInfo renderSignal = typeof(HUDWireContentsHelper).GetMethod("RenderSignal", BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance,
+                binder: null,
+                types: new[] { typeof(ISignal) },
+                modifiers: null)!;
+
+            _renderSignalHook = new Hook(
+                renderSignal,
+                (Action<Action<HUDWireContentsHelper, ISignal>, HUDWireContentsHelper, ISignal>)Patch);
+        }
+
+        private static void Patch(Action<HUDWireContentsHelper, ISignal> orig, HUDWireContentsHelper contentsHelper, ISignal signal)
+        {
+            if (signal is BeltItemSignal beltItemSignal && beltItemSignal.Value is FluidPackageItem)
+            {
+                return;
+            }
+            
+            orig(contentsHelper, signal);
+        }
+
+        public void Dispose()
+        {
+            _renderSignalHook?.Dispose();
+        }
+    }
+}
