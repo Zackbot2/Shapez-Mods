da4a816bb15da5ffbfeed4b240178a3cd5867f49
Zack <zacharycummings0714@gmail.com>
2026-08-01 20:29:09 -0600

    Create DisplayCopy mod

---
+++ DisplayCopy/DisplayCopyMod.cs
@@ -0,0 +1,20 @@
+﻿using Core.Logging;
+using System;
+
+namespace DisplayCopy
+{
+    public class DisplayCopyMod : IMod
+    {
+        internal static ILogger Logger { get; private set; } = null!;
+
+        public DisplayCopyMod(ILogger logger)
+        {
+            Logger = logger;
+        }
+
+        public void Dispose()
+        {
+
+        }
+    }
+}
